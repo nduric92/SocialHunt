66499fbb7ed85d179f2d65b64f6db24d979339c4
Nemanja Duric <117755538+nduric92@users.noreply.github.com>
2023-10-13 13:25:22 +0200

    pathchange

--- socialhunt.hr/hunt.sql
+++ socialhunt.hr/hunt.sql
@@ -2,7 +2,7 @@ drop database if exists hunt;
 create database hunt;
 use hunt;
 
-# c:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8mb4 < C:\Users\djuki\Documents\GitHub\SocialHunt.hr\hunt.sql
+# c:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8mb4 < C:\Users\djuki\Documents\GitHub\SocialHunt\socialhunt.hr\hunt.sql
 
 #alter database persefona_proizvodnja charset utf8mb4;(ISPRAVI - za server koji smo koristili)
 
